53d80ca05b1c1b5498b0663f5b201805f28e669c
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-03-05 23:30:17 +0530

    custom hook

--- src/components/useStateTrack.js
+++ src/components/useStateTrack.js
@@ -2,17 +2,20 @@ import { useEffect, useState } from "react";
 
 const useStateTrack = (songUrls) => {
   const [tracks, setTracks] = useState(null);
+  // const [songsBuffer, setSongsBuffer] = useState([]);
 
   async function loadAudioFile(audioContext, url) {
-    console.log("url", url);
+    console.log("loadurl", url);
     const response = await fetch(url);
     const arrayBuffer = await response.arrayBuffer();
-    console.log("arrayBuffer", arrayBuffer);
     const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
     return audioBuffer;
   }
 
+  console.log("tracks", tracks);
+
   async function createSource(url) {
+    console.log("funUrl", url);
     const audioContext = new AudioContext();
     const audioBuffer = await loadAudioFile(audioContext, url);
 
@@ -27,16 +30,16 @@ const useStateTrack = (songUrls) => {
   }
 
   useEffect(() => {
-    async function processArray() {
-      const promises = songUrls.map(async (url) => {
+    async function processSongUrls() {
+      const createSourcePromise = songUrls.map(async (url) => {
         return createSource(url);
       });
-      const promise = await Promise.all(promises);
+      const promise = await Promise.all(createSourcePromise);
       console.log("promise", promise);
       setTracks(promise);
     }
 
-    processArray();
+    processSongUrls();
   }, [songUrls, setTracks]);
 
   return tracks;
