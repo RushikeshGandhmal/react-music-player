9ab841a509d6b7b4e4d428c81797e14851b8ec26
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-03-06 18:18:04 +0530

    removed code

--- src/components/AudioMixer.jsx
+++ src/components/AudioMixer.jsx
@@ -1,35 +1,22 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from "react";
 import useStateTrack from "./useStateTrack";
 
-const AudioMixer = ({ songs, play, pause }) => {
+const AudioMixer = ({ songs }) => {
   const { tracks, playTrack, pauseTrack } = useStateTrack(songs);
-
   //   console.log("ccc", tracks, playTrack, pauseTrack);
 
   const handlePlay = () => {
     tracks?.forEach((track, index) => {
-      //   track.source.start();
       playTrack(index);
     });
   };
 
   const handlePause = () => {
     tracks?.forEach((track, index) => {
-      //   track.source.stop();
       pauseTrack(index);
     });
   };
 
-  useEffect(() => {
-    if (play && !pause) {
-      // Play the all audio tracks
-      //   handlePlay();
-    } else if (!play && pause) {
-      // Pause all tracks
-      //   handlePause();
-    }
-  }, [play, pause, tracks]);
   return (
     <>
       <button
